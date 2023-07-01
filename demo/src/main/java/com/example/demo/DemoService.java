94863fcfed0c9194afe8d94422415ca50c96ebfc
Elendira <e.crimsonnail@gmail.com>
2023-07-01 10:52:49 +0900

    questions2 added

--- demo/src/main/java/com/example/demo/DemoService.java
+++ demo/src/main/java/com/example/demo/DemoService.java
@@ -1,14 +1,12 @@
 package com.example.demo;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.querydsl.core.Tuple;
 import com.querydsl.jpa.JPQLQueryFactory;
 import com.querydsl.sql.SQLQueryFactory;
 
@@ -30,20 +28,9 @@ public class DemoService {
         logger.info("JPQLQueryFactory: "+userList.toString());
 
         // SQLQueryFactory is NG
-        List<Tuple> list = sqlQueryFactory.select(
-                qUser.id, qUser.name, qUser.createdBy, qUser.createdAt)
+        userList = sqlQueryFactory.select(qUser)
                 .from(qUser)
                 .fetch(); // error here
-        userList = list.stream()
-            .map(tuple -> {
-                User user = User.builder()
-                    .id(tuple.get(qUser.id))
-                    .name(tuple.get(qUser.name))
-                    .createdBy(tuple.get(qUser.createdBy))
-                    .createdAt(tuple.get(qUser.createdAt))
-                    .build();
-                return user;
-            }).collect(Collectors.toList());
         logger.info("SQLQueryFactory: "+userList.toString());
         return userList;
     }
